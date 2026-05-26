124f987b84d5c3b0eb48234797df79cedf7128e7
@luis.albuquerque <luis.albuquerque@alu.ufc.br>
2026-05-26 00:11:00 -0300

    chore: remove temporary and unused files from project root

--- set-admin.sql
+++
@@ -1,8 +0,0 @@
--- Verificar se o usuário existe
-SELECT id, email, username, "isAdmin" FROM "User" WHERE email = 'teste2@gmail.com';
-
--- Tornar o usuário admin
-UPDATE "User" SET "isAdmin" = true WHERE email = 'teste2@gmail.com';
-
--- Verificar se funcionou
-SELECT id, email, username, "isAdmin" FROM "User" WHERE email = 'teste2@gmail.com';
